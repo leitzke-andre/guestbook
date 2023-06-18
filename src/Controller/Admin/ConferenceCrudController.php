f1f973d1af73f300179d4863bf5b03fe4d9c141f
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2023-06-18 20:26:28 +0200

    Added filters for city and isInternational to ConferenceCrudController

--- src/Controller/Admin/ConferenceCrudController.php
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -3,13 +3,23 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\BooleanFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
 
 class ConferenceCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Conference::class;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(TextFilter::new('city'))
+            ->add(BooleanFilter::new('isInternational'));
     }
 
    /*
